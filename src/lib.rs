f86a085b546775d744e5a286cd4e2ff1871f4bf9
iloureiro <ignacio.loureiro@endava.com>
2018-09-25 17:51:11 -0300

    bucketizer

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+#[derive(Clone, Debug, PartialEq)]
+struct Bucketizer {
+    buckets: Vec<Bucket>,
+}
+
+type Bucket = (Option<f64>, Option<f64>, f64);
+
+impl Bucketizer {
+    fn new() -> Self {
+        //Self es lo mismo que Bucketizer
+        Bucketizer {
+            buckets: Vec::new(),
+        }
+    }
+
+    fn add_bucket(&mut self, min: Option<f64>, max: Option<f64>, value: f64) {}
+
+    fn bucketize(&self, input: f64) -> Option<f64> {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Bucketizer;
+
+    #[test]
+    fn single_bucket_middle_values() {
+        let mut bucketizer = Bucketizer::new();
+        bucketizer.add_bucket(Some(0.0), Some(1.0), 0.5);
+        assert_eq!(bucketizer.bucketize(0.1), Some(0.5));
+        assert_eq!(bucketizer.bucketize(999.999), None);
+    }
+
+    #[test]
+    fn single_bucket_end_values() {
+        let mut bucketizer = Bucketizer::new();
+        bucketizer.add_bucket(Some(0.0), Some(1.0), 0.5);
+        assert_eq!(bucketizer.bucketize(0.0), Some(0.5));
+        assert_eq!(bucketizer.bucketize(1.0), None);
+    }
+}
