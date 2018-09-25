e2a3d274e744efe53f18fcf138dc42c21913d99d
driden <776982+driden@users.noreply.github.com>
2018-09-25 19:03:17 -0300

    Tests de bucketizer

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ impl Bucketizer {
         }
     }
 
-    fn bucket(self, min: Option<f64>, max: Option<f64>, value: f64) {
+    fn bucket(self, min: Option<f64>, max: Option<f64>, value: f64) -> Bucketizer{
         self
     }
 
@@ -28,17 +28,36 @@ mod tests {
 
     #[test]
     fn single_bucket_middle_values() {
-        let bucketizer = Bucketizer::new()
-            .bucket(Some(0.0), Some(1.0), 0.5);
-        assert_eq!(bucketizer.bucketize((0.1), Some(0.5));
-        assert_eq!(bucketizer.bucketize((999.999), None);
+        let bucketizer = Bucketizer::new().bucket(Some(0.0), Some(1.0), 0.5);
+        assert_eq!(bucketizer.bucketize(0.1), Some(0.5));
+        assert_eq!(bucketizer.bucketize(999.999), None);
     }
 
     #[test]
     fn single_bucket_end_values() {
-        let bucketizer = Bucketizer::new()
-            .bucket(Some(0.0), Some(1.0), 0.5);
+        let bucketizer = Bucketizer::new().bucket(Some(0.0), Some(1.0), 0.5);
         assert_eq!(bucketizer.bucketize(0.0), Some(0.5));
         assert_eq!(bucketizer.bucketize(1.0), None);
+    }
+
+    #[test]
+    fn multiple_buckets_closed_ends() {
+        let b =
+            Bucket::new()
+                .bucket(Some(-1.0), Some(0, 0), -0.5)
+                .bucket(Some(0.0), Some(1.0), 0.5);
+        assert!(b.bucketize(0.0), Some(0.5));
+        assert!(b.bucketize(-0.7), Some(-0.5));
+        assert!(b.bucketize(9999.0), None);
+    }
+
+    #[test]
+    fn multiple_buckets_opened_ends() {
+        let b = Bucket::new()
+            .bucket(Some(0.0), Some(1, 0), 0.5)
+            .bucket(Some(1.0), None, 1.5);
+        assert!(b.bucketize(0.0), Some(0.5));
+        assert!(b.bucketize(-0.7), None);
+        assert!(b.bucketize(9999.0), Some(1.5));
     }
 }
